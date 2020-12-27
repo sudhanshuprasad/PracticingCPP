ade2bdd68c30ee98dc783c07f29858b0fba8a824
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2020-12-27 12:23:16 +0530

    prime factor

--- SieveofEratoshenes.cpp
+++ SieveofEratoshenes.cpp
@@ -20,11 +20,37 @@ void primeSieve(int n){
     }cout<<endl;
 }
 
+
+void primefactor(int n){
+
+    int spf[100]={0};
+    for(int i=2;i<=n;i++){
+        spf[i]=i;
+    }
+
+    for(int i=2;i<=n;i++){
+        if(spf[i]==i){
+            for(int j=i*i;j<=n;j+=i){
+                if(spf[j]==j){
+                    spf[j]=i;
+                }
+            }
+        }
+    }
+
+    while(n!=1){
+        cout<<spf[n]<<" ";
+        n=n/spf[n];
+    }
+}
+
+
 int main(){
     
-    int n;
-    cin>>n;
-    primeSieve(n);
+    int a,b;
+    cin>>a>>b;
+    primeSieve(a);
+    primefactor(b);
 
     return 0;
 }
